b78398acae610b5de4cf81a7827b984a900ad928
sv <sv@0ban.(none)>
2013-09-15 13:16:15 +0600

    add argv

--- config.js
+++ config.js
@@ -6,31 +6,30 @@
 **
 */
 
-var fs = require('fs');
+var fs = require('fs'),
+    argv = require('optimist').argv;
 
 var config = {
   add_proxy_header: true,//activate addition of X-Forwarded-For header for better logging on real server side
   allow_ip_list: './config/allow_ip_list',
   black_list:    './config/black_list',
   host_filters:   './config/hostfilters.js',
-  listen:[{ip:'0.0.0.0', port:80},//all ipv4 interfaces
-          {ip:'::', port:80}],//all ipv6 interfaces
-  listen_ssl:[{
+  listen:[{ip:'0.0.0.0', port: argv.port || 8080},//all ipv4 interfaces
+          {ip:'::', port: argv.port6 || 8081}],//all ipv6 interfaces
+  listen_ssl:[/*{
               ip:'0.0.0.0',//all *secure* ipv4 interfaces
-              port:443,
-              key:fs.readFileSync('/path/to/ssl.key'),
-              cert:fs.readFileSync('/path/to/ssl.crt'),
-              ca:[fs.readFileSync('/path/to/ca.pem'), 
-                  fs.readFileSync('/path/to/sub-ca.pem')]
+              port:8443,
+              key:fs.readFileSync('/home/sv/ssl/server.key'),
+              cert:fs.readFileSync('/home/sv/ssl/server.crt'),
+              ca:[fs.readFileSync('/home/sv/ssl/server.pem')]
             },{ 
               ip:'::',//all *secure* ipv6 interfaces
-              port:443,
-              key:fs.readFileSync('/path/to/ssl.key'),
-              cert:fs.readFileSync('/path/to/ssl.crt'),
-              ca:[fs.readFileSync('/path/to/ca.pem'), 
-                  fs.readFileSync('/path/to/sub-ca.pem')]
+              port:8443,
+              key:fs.readFileSync('/home/sv/ssl/server.key'),
+              cert:fs.readFileSync('/home/sv/ssl/server.crt'),
+              ca:[fs.readFileSync('/home/sv/ssl/server.pem')]
 
-            }   
+            }  */
            ]
 };
 
